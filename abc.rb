35d83275e68b5366c6dc94f48eff8ec72cffe49a
Scott <scott@cloudspace.com>
2012-06-08 14:27:39 -0400

    first change

--- abc.rb
+++ abc.rb
@@ -1,14 +1,1 @@
-# here is the start of my file
-# another change
-# another change2
-# another change3
-# another change4
-# another change5
-# another change6
-# another change7
-# another change8
-# another change9
-# another change10
-# another change11
-# another change12
-# another change13
+change 1
